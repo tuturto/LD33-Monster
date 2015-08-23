3301489f246927af01480c4dba1e689073d7726a
Tuukka Turto <tuukka.turto@oktaeder.net>
2015-08-23 23:36:03 +0300

    Add invincibility for player

--- LD33/Game.fs
+++ LD33/Game.fs
@@ -34,13 +34,17 @@ type Tree =
 
 let R = System.Random()
 
-let initialPlayerState = {x = 100.0f;
-                          y = 464.0f;
-                          vx = 0.0f;
-                          vy = 0.0f; }
+type PlayerState =
+    | Invisible of int * Mob
+    | Normal of Mob
+
+let initialPlayerState = Invisible(2000, {x = 100.0f;
+                                          y = 464.0f;
+                                          vx = 0.0f;
+                                          vy = 0.0f; })
 
 let playerStream =
-    new BehaviorSubject<Mob>(initialPlayerState)
+    new BehaviorSubject<PlayerState>(initialPlayerState)
 
 let fireStream =
     new BehaviorSubject<Mob list>([])
@@ -50,6 +54,9 @@ let scoreStream =
 
 let highScoreStream = 
     new BehaviorSubject<int>(0)
+
+let creditsStream =
+    new BehaviorSubject<int>(3)
 
 let forestTextures = ["forest_1"; "forest_2"; "forest_3"; "forest_4"; "forest_5"; "forest_6"; "forest_7"; "forest_8"]
 
@@ -75,7 +82,9 @@ let torcherStream =
 
 RxNA.Input.gameTimeStream
 |> Observable.subscribe (fun time ->
-    let player = playerStream.Value
+    let player = match playerStream.Value with
+                     | Invisible (_, p) -> p
+                     | Normal p -> p
     let newVY = if player.y > 464.0f && player.vy >= 0.0f then 0.0f
                     else player.vy + 12.5f * (float32)time.ElapsedGameTime.TotalSeconds
     let newY = player.y + newVY
@@ -86,11 +95,20 @@ RxNA.Input.gameTimeStream
         if player.x + newVX * (float32)time.ElapsedGameTime.TotalSeconds * 50.5f < -32.0f then -32.0f
             else if player.x + newVX * (float32)time.ElapsedGameTime.TotalSeconds * 50.5f > 704.0f then 704.0f
                 else player.x + newVX * (float32)time.ElapsedGameTime.TotalSeconds * 50.5f
-    playerStream.OnNext {player with y = newY;
-                                     vy = newVY;
-                                     x = newX;
-                                     vx = newVX;}
-    ) |> ignore
+    match playerStream.Value with
+                     | Invisible (t, p) -> playerStream.OnNext <| Invisible (t, {player with y = newY; vy = newVY; x = newX; vx = newVX;})
+                     | Normal p -> playerStream.OnNext <| Normal {player with y = newY; vy = newVY; x = newX; vx = newVX;})
+|> ignore
+
+let setJumpOn player =
+    match player with
+        | Invisible (t, p) -> playerStream.OnNext <| Invisible (t, {p with vy = -10.0f;})
+        | Normal p -> playerStream.OnNext <| Normal {p with vy = -10.0f;}
+
+let setLateralSpeed player speed =
+    match player with
+        | Invisible (t, p) -> playerStream.OnNext <| Invisible (t, {p with vx = speed;})
+        | Normal p -> playerStream.OnNext <| Normal {p with vx = speed;}
 
 RxNA.Input.keysPressedStream
 |> Observable.filter (fun x -> gameModeStream.Value = GameOn)
@@ -98,14 +116,17 @@ RxNA.Input.keysPressedStream
     (fun x -> if x.Contains Keys.Escape then 
                     gameModeStream.OnNext MainMenuShown
               if x.Contains Keys.Space then
-                    let player = playerStream.Value
-                    if player.y >= 464.0f then playerStream.OnNext {player with vy = -10.0f;}
+                    let playerState = playerStream.Value
+                    let player = match playerState with
+                                     | Invisible (_, p) -> p
+                                     | Normal p -> p
+                    if player.y >= 464.0f then setJumpOn playerState
               if x.Contains Keys.Left then
-                    let player = playerStream.Value
-                    playerStream.OnNext {player with vx = -4.0f}
+                    let playerState = playerStream.Value
+                    setLateralSpeed playerState -4.0f
               if x.Contains Keys.Right then
-                    let player = playerStream.Value
-                    playerStream.OnNext {player with vx = 4.0f})
+                    let playerState = playerStream.Value
+                    setLateralSpeed playerState 4.0f)
 |> ignore  
 
 RxNA.Input.keyDownStream
@@ -114,7 +135,8 @@ RxNA.Input.keyDownStream
     (fun x -> match x with
                   | Keys.Escape -> gameModeStream.OnNext MainMenuShown
                   | Keys.Space -> gameModeStream.OnNext MainMenuShown
-                  | _ -> ()) |> ignore
+                  | _ -> ())
+|> ignore
 
 
 
@@ -125,7 +147,8 @@ gameModeStream
                                  {x=500.0f; y=350.0f; vx=10.0f; vy=0.0f}
                                  {x=700.0f; y=300.0f; vx=10.0f; vy=0.0f}
                                  {x=900.0f; y=250.0f; vx=10.0f; vy=0.0f}
-                                 {x=1100.0f; y=200.0f; vx=10.0f; vy=0.0f}])) |> ignore
+                                 {x=1100.0f; y=200.0f; vx=10.0f; vy=0.0f}]))
+|> ignore
 
 let intersecting x1 y1 x2 y2 distance =
     let dx = (x1+32.f)-(x2+32.f)
@@ -146,11 +169,14 @@ RxNA.Input.gameTimeStream
 |> Observable.subscribe
     (fun gameTime -> 
         let fireSpeed = (float32)gameTime.ElapsedGameTime.TotalSeconds * 7.5f
-        let player = playerStream.Value
+        let player = match playerStream.Value with
+                         | Invisible (_, p) -> p
+                         | Normal p -> p
         fireStream.OnNext (fireStream.Value |> List.map (fun fire -> if isMobPastScreen fire then newFire()
                                                                        else if intersecting fire.x fire.y player.x player.y 64.0f then scoreStream.OnNext(scoreStream.Value + 1)
                                                                                                                                        newFire()
-                                                                           else {fire with x = fire.x - fire.vx * fireSpeed}))) |> ignore
+                                                                           else {fire with x = fire.x - fire.vx * fireSpeed})))
+|> ignore
 
 let isTreePastScreen (tree:Tree) =
     tree.x < -200.0f
@@ -166,7 +192,8 @@ RxNA.Input.gameTimeStream
     (fun gameTime ->
         let forestSpeed = (float32)gameTime.ElapsedGameTime.TotalSeconds * 7.5f
         forestStream.OnNext (forestStream.Value |> List.map (fun tree -> if isTreePastScreen tree then newTree()
-                                                                            else {tree with x = tree.x - tree.vx * forestSpeed}))) |> ignore
+                                                                            else {tree with x = tree.x - tree.vx * forestSpeed})))
+|> ignore
 
 let newTorcher() = 
     { x = 864.0f + (float32)(R.NextDouble()) * 100.0f;
@@ -178,24 +205,36 @@ RxNA.Input.gameTimeStream
 |> Observable.filter (fun x -> gameModeStream.Value = GameOn || gameModeStream.Value = GameOver)
 |> Observable.subscribe
     (fun gameTime -> 
-        let mobSpeed = (float32)gameTime.ElapsedGameTime.TotalSeconds * 7.5f
-        let player = playerStream.Value
+        let mobSpeed = (float32)gameTime.ElapsedGameTime.TotalSeconds * 7.5f        
         torcherStream.OnNext (torcherStream.Value |> List.map (fun mob -> if isMobPastScreen mob then newTorcher()
                                                                            else {mob with x = mob.x - mob.vx * mobSpeed}))
-        torcherStream.Value |> List.iter (fun mob -> if intersecting player.x player.y mob.x mob.y 25.0f then gameModeStream.OnNext GameOver)) |> ignore
+        torcherStream.Value |> List.iter (fun mob -> match playerStream.Value with
+                                                         | Invisible _ -> ()
+                                                         | Normal player -> if intersecting player.x player.y mob.x mob.y 25.0f
+                                                                                then creditsStream.OnNext <| creditsStream.Value - 1
+                                                                                     if creditsStream.Value = 0 then gameModeStream.OnNext GameOver
+                                                                                        else playerStream.OnNext initialPlayerState))
+|> ignore
 
 scoreStream
 |> Observable.filter (fun x -> x > highScoreStream.Value)
 |> Observable.subscribeObserver highScoreStream
 |> ignore
 
-RxNA.Renderer.renderStream
-|> Observable.filter (fun x -> gameModeStream.Value = GameOn || gameModeStream.Value = GameOver)
+let gameOnRendering = RxNA.Renderer.renderStream
+                      |> Observable.filter (fun x -> gameModeStream.Value = GameOn)
+let gameOverRendering = RxNA.Renderer.renderStream
+                        |> Observable.filter (fun x -> gameModeStream.Value = GameOver)
+let gameRendering = RxNA.Renderer.renderStream
+                    |> Observable.filter (fun x -> gameModeStream.Value = GameOn || gameModeStream.Value = GameOver)
+
+gameRendering
 |> Observable.subscribe
     (fun res ->
         List.iter (fun item -> res.spriteBatch.Draw(res.textures.Item item.texture ,
                                                     Vector2(item.x, item.y),
-                                                    Color.White)) forestStream.Value) |> ignore
+                                                    Color.White)) forestStream.Value)
+|> ignore
 
 RxNA.Renderer.renderStream
 |> Observable.subscribe
@@ -206,8 +245,8 @@ RxNA.Renderer.renderStream
         List.iter (fun item -> (match item with
                                     | index, value -> res.spriteBatch.Draw(res.textures.Item value,
                                                                            Vector2((float32)index*64.0f + leftmost, 5.0f),
-                                                                           Color.White))) scoreList
-        ) |> ignore
+                                                                           Color.White))) scoreList)
+|> ignore
 
 RxNA.Renderer.renderStream
 |> Observable.subscribe
@@ -217,11 +256,19 @@ RxNA.Renderer.renderStream
         List.iter (fun item -> (match item with
                                     | index, value -> res.spriteBatch.Draw(res.textures.Item value,
                                                                            Vector2((float32)index*64.0f + 5.0f, 5.0f),
-                                                                           Color.White))) scoreList
-        ) |> ignore
+                                                                           Color.White))) scoreList)
+|> ignore
 
-RxNA.Renderer.renderStream
-|> Observable.filter (fun x -> gameModeStream.Value = GameOn)
+gameOnRendering
+|> Observable.subscribe
+    (fun res ->
+        let credits = creditsStream.Value
+        for i in [0..credits] do (res.spriteBatch.Draw(res.textures.Item "monster_f1",
+                                                       Vector2((float32)i * 64.0f + 5.0f, 64.0f),
+                                                       Color.White)))
+|> ignore
+
+gameOnRendering
 |> Observable.subscribe
     (fun res ->
         let frame = int(res.gameTime.TotalGameTime.TotalMilliseconds / 250.0) % 4
@@ -234,39 +281,41 @@ RxNA.Renderer.renderStream
 
         fireStream.Value |> List.iter (fun fire -> res.spriteBatch.Draw(texture,
                                                                         Vector2(fire.x+32.0f, fire.y-64.0f),
-                                                                        Color.White))
-        ) |> ignore
+                                                                        Color.White)))
+|> ignore
 
-RxNA.Renderer.renderStream
+gameRendering
 |> Observable.subscribe (fun res -> 
-    if gameModeStream.Value = GameOn || gameModeStream.Value = GameOver then
-        let frame = int(res.gameTime.TotalGameTime.TotalMilliseconds / 250.0) % 4
-        let texture =  match frame with 
-                           | 0 -> res.textures.Item "torcher_1"
-                           | 1 -> res.textures.Item "torcher_2"
-                           | 2 -> res.textures.Item "torcher_3"
-                           | 3 -> res.textures.Item "torcher_4"
-                           | _ -> res.textures.Item ""
+    let frame = int(res.gameTime.TotalGameTime.TotalMilliseconds / 250.0) % 4
+    let texture =  match frame with 
+                       | 0 -> res.textures.Item "torcher_1"
+                       | 1 -> res.textures.Item "torcher_2"
+                       | 2 -> res.textures.Item "torcher_3"
+                       | 3 -> res.textures.Item "torcher_4"
+                       | _ -> res.textures.Item ""
 
-        torcherStream.Value |> List.iter (fun item -> res.spriteBatch.Draw(texture,
-                                                                           Vector2(item.x+32.0f, item.y-64.0f),
-                                                                           Color.White))) |> ignore
+    torcherStream.Value |> List.iter (fun item -> res.spriteBatch.Draw(texture,
+                                                                       Vector2(item.x+32.0f, item.y-64.0f),
+                                                                       Color.White)))
+|> ignore
 
-RxNA.Renderer.renderStream
+gameOnRendering
 |> Observable.subscribe (fun res -> 
-    if gameModeStream.Value = GameOn then
-        let frame = int(res.gameTime.TotalGameTime.TotalMilliseconds / 250.0) % 4
-        let texture =  match frame with 
-                           | 0 -> res.textures.Item "monster_f1"
-                           | 1 -> res.textures.Item "monster_f2"
-                           | 2 -> res.textures.Item "monster_f3"
-                           | 3 -> res.textures.Item "monster_f4"
-                           | _ -> res.textures.Item ""
+    let frame = int(res.gameTime.TotalGameTime.TotalMilliseconds / 250.0) % 4
+    let texture =  match frame with 
+                       | 0 -> res.textures.Item "monster_f1"
+                       | 1 -> res.textures.Item "monster_f2"
+                       | 2 -> res.textures.Item "monster_f3"
+                       | 3 -> res.textures.Item "monster_f4"
+                       | _ -> res.textures.Item ""
 
-        let player = playerStream.Value
-        res.spriteBatch.Draw(texture,
-                             Vector2(player.x + 32.0f, player.y - 64.0f),
-                             Color.White)) |> ignore
+    let player = match playerStream.Value with
+                     | Invisible (_, p) -> p
+                     | Normal p -> p
+    res.spriteBatch.Draw(texture,
+                         Vector2(player.x + 32.0f, player.y - 64.0f),
+                         Color.White))
+|> ignore
 
 type Game () as this =
     inherit Microsoft.Xna.Framework.Game()
@@ -295,6 +344,7 @@ type Game () as this =
                           | Keys.Space -> playerStream.OnNext initialPlayerState
                                           torcherStream.OnNext initialTorchers
                                           scoreStream.OnNext 0
+                                          creditsStream.OnNext 3
                                           gameModeStream.OnNext GameOn
                           | _ -> ()) |> ignore 
  
